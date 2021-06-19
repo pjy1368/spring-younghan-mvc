2b8d182032e569df7b55c80d15fdfaf008c404d7
JinYoung Park <basejin0810@naver.com>
2021-06-19 14:15:18 +0900

    feat(RequestBodyStringServlet): HTTP 요청 데이터 - API 메시지 바디 - 단순 텍스트

---
+++ src/main/java/hello/servlet/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,21 @@
+package hello.servlet.basic.request;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final String messageBody = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+        response.getWriter().write("ok");
+    }
+}
