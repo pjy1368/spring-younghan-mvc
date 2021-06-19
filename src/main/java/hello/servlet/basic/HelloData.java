959a831fbb78e43dc8276c9a4483d9498e301b7c
JinYoung Park <basejin0810@naver.com>
2021-06-19 14:26:43 +0900

    feat(RequestBodyJsonServlet): HTTP 요청 데이터 - API 메시지 바디 - JSON

---
+++ src/main/java/hello/servlet/basic/HelloData.java
@@ -0,0 +1,14 @@
+package hello.servlet.basic;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class HelloData {
+
+    private String username;
+    private int age;
+}
---
+++ src/main/java/hello/servlet/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hello.servlet.basic.HelloData;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.util.StreamUtils;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final String messageBody = StreamUtils.copyToString(request.getInputStream(), StandardCharsets.UTF_8);
+        System.out.println("messageBody = " + messageBody);
+        final HelloData helloData = objectMapper.readValue(messageBody, HelloData.class);
+        System.out.println("helloData.username = " + helloData.getUsername());
+        System.out.println("helloData.age = " + helloData.getAge());
+    }
+}
